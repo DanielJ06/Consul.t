b599a66d6179b5043c150eff3fae7f399ff9672b
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-05-18 11:34:20 -0300

    Design login screen

--- app/src/main/java/com/ioasys/diversidade/ui/MainActivity.kt
+++ app/src/main/java/com/ioasys/diversidade/ui/MainActivity.kt
@@ -2,9 +2,13 @@ package com.ioasys.diversidade.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.ioasys.diversidade.R
+import com.ioasys.diversidade.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +19,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        navHostFragment.findNavController().setGraph(R.navigation.auth_graph)
 
         navController = findNavController(R.id.navHostFragment)
     }
